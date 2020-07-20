63873da20b95849a73376a1d052f13ef3cc32615
yolka85 <4yolka@gmail.com>
2020-07-20 18:44:56 +0300

    Updated datepicker page

--- scripts/datepicker.js
+++ scripts/datepicker.js
@@ -7,7 +7,20 @@ $("#datepicker").flatpickr({
     altFormat: "j F Y, H:i",
     dateFormat: "d-m-Y H:i",
     minDate: "today",
+    inline: true,
     locale: {
         firstDayOfWeek: 1
     }
+});
+$('#save-date-button').click(function(){
+    var ThisDate = new Date(),
+        enteredDate = $("#datepicker").val();
+    if(enteredDate <= ThisDate){
+        $('.form-error').css('display', 'block');
+    }else {
+        $('.form-error').css('display', 'none');
+        var url = "https://m.me/103229161431509?ref=" + enteredDate.replace(/ /g, '-');
+        $(location).attr('href',url);
+    }
+    
 });
