45209b1af511eebfc128d0d997b83e891fafb510
yolka85 <4yolka@gmail.com>
2020-07-23 20:12:18 +0300

    Added input masks and more-details block

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -22,7 +22,87 @@ $('#button-send').click(function() {
     } else {
         $(location).attr('href', url+'n');
     }
-})
+});
+
+$('#send-btn').click(function() {
+    $(".form-control").removeClass('input-error');
+    $('.error-message').hide();
+    var incompleteInputs = $(".form-control").filter (function () {
+        return !$(this).inputmask("isComplete") 
+    });
+    if (incompleteInputs.length > 0) {
+        $('.error-message').show();
+        incompleteInputs.each(function(){
+            $(this).addClass('input-error');
+        })
+    } else {
+        var url = "https://m.me/103229161431509?ref=6";
+        $(location).attr('href',url);
+    } 
+});
 
 // ----Input mask----
+$(document).ready(function(){
+    $('#doc1-series').inputmask({
+        mask: "9999",
+        placeholder: "1234"
+    });
+    $('#doc1-number').inputmask({
+        mask: "999999",
+        placeholder: "123456"
+    });
+    $('#doc2-number').inputmask({
+        mask: "9{11}",
+        placeholder: "12345678900"
+    });
+});
+$('#doc-type1').change(function () {
+    if($('#doc-type1').val() === "passport") {
+        $('#doc1-series').inputmask({
+            mask: "9999",
+            placeholder: "1234"
+        });
+        $('#doc1-number').inputmask({
+            mask: "999999",
+            placeholder: "123456"
+        });
+    }else if($('#doc-type1').val() === "doc2") {
+        $('#doc1-series').inputmask({
+            mask: "9",
+            placeholder: "1"
+        });
+        $('#doc1-number').inputmask({
+            mask: "9",
+            placeholder: "1"
+        });
+    }
+});
+$('#doc-type2').change(function () {
+    if($('#doc-type2').val() === "snils") {
+        $('#doc2-number').inputmask({
+            mask: "9{11}",
+            placeholder: "12345678900"
+        });
+    }else if($('#doc-type2').val() === "inn") {
+        $('#doc2-number').inputmask({
+            mask: "(9{9,10})|(9{12})",
+            placeholder: "123456789001"
+        });
+    }else if($('#doc-type2').val() === "oms") {
+        $('#doc2-number').inputmask({
+            mask: "9{16}",
+            placeholder: "5678123456789001"
+        });
+    }
+})
 $(":input").inputmask();
+
+$('#details').click(function (e) {
+    e.preventDefault();
+    $('.identification').hide();
+    $('.more-details').show();
+});
+$('#back-btn').click(function () {
+    $('.more-details').hide();
+    $('.identification').css('display', 'flex');
+})
